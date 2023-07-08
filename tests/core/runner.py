56ac85e54f11d5b22524ee9f601668044dae125e
James M. Putnam <putnamjm.design@gmail.com>
2023-07-08 16:22:14 -0400

    #60 - core: load image

--- tests/core/runner.py
+++ tests/core/runner.py
@@ -6,27 +6,7 @@ with open(sys.argv[1]) as f: test_source = f.readlines()
 
 def runtest(test, expected):
     proc = subprocess.Popen(['../dist/runtime',
-                             '-l../src/core/core.l',
-	                     '-l../src/core/closures.l',
-	                     '-l../src/core/fixnums.l',
-	                     '-l../src/core/read-macro.l',
-	                     '-l../src/core/read.l',
-	                     '-l../src/core/sequences.l',
-	                     '-l../src/core/symbol-macro.l',
-	                     '-l../src/core/symbols.l',
-	                     '-l../src/core/vectors.l',
-                             '-l../src/core/compile.l',
-                             '-l../src/core/exceptions.l',
-                             '-l../src/core/format.l',
-                             '-l../src/core/lambda.l',
-                             '-l../src/core/lists.l',
-                             '-l../src/core/load.l',
-                             '-l../src/core/macro.l',
-                             '-l../src/core/parse.l',
-                             '-l../src/core/perf.l',
-                             '-l../src/core/quasiquote.l',
-                             '-l../src/core/streams.l',
-                             '-l../src/core/strings.l',
+                             '-l../dist/core.l',
                              '-p',
                              '-e' + test],\
                             stdout=subprocess.PIPE,\
