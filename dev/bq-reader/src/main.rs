c153e18fe3489bdf4e0d9c8422aa4b373bbe5141
James Putnam <putnamjm.design@gmail.com>
2023-12-20 11:14:22 -0500

    #771 - mu: lists now loop in backquote redesign

--- dev/bq-reader/src/main.rs
+++ dev/bq-reader/src/main.rs
@@ -1,86 +1,24 @@
-#![allow(dead_code)]
+mod bq_machine;
 
-use {rust_fsm::*, std::collections::VecDeque};
+use std::{
+    io,
+    io::BufRead
+};
 
-state_machine! {
-    derive(Debug)
-    repr_c(true)
-    Reader(Backquote)
+use crate::bq_machine::BqMachine;
 
-    // `
-        Backquote => {
-            At => SyntaxError [ At ],
-            Backquote => Backquote,
-            Comma => Comma,
-            Constant => Form,
-            Dot => SyntaxError [ Dot ],
-            EndList => Exit,
-            List => List,
-            Symbol => Quote,
-        },
-
-    // `,
-        Comma => {
-            At => SyntaxError [ At ],
-            Backquote => Backquote,
-            Comma => Comma,
-            Constant => Form,
-            Dot => SyntaxError [ Dot ],
-            EndList => Exit,
-            List => List,
-            Symbol => Form,
-        },
-
-    // `(
-        List => {
-            At => SyntaxError [ At ],
-            Backquote => Backquote,
-            Comma => ListComma,
-            Constant => Form,
-            Dot => SyntaxError [ Dot ],
-            EndList => Exit,
-            List => List,
-            Symbol => Quote,
-        },
-    // `,(
-        ListComma => {
-            At => SyntaxError [ At ],
-            Backquote => Backquote,
-            Comma => ListComma,
-            Constant => Form,
-            Dot => SyntaxError [ Dot ],
-            EndList => Exit,
-            List => List,
-            Symbol => Quote,
-        },
+fn tests() {
+    BqMachine::parse("`,@".to_string());
+    BqMachine::parse("`,foo ".to_string());
+    BqMachine::parse("`,(1234 )".to_string());
+    BqMachine::parse("`,(think 123 thank thunk )".to_string());
 }
 
 fn main() {
-    let mut machine: StateMachine<Reader> = StateMachine::new();
-
-    let mut next_states =
-        VecDeque::<ReaderInput>::from_iter(vec![ReaderInput::Comma,
-                                                ReaderInput::Constant],
-        );
-
-    loop {
-        if next_states.is_empty() {
-            break;
-        }
-
-        let output = machine.consume(&next_states.pop_front().unwrap());
-        println!("state {:?}", machine.state());
-
-        match machine.state() {
-            ReaderState::Backquote | ReaderState::Form | ReaderState::Exit | ReaderState::Quote => {
-                println!("way to go, funbuns")
-            }
-
-            ReaderState::SyntaxError => {
-                println!("syntax error {:?}", output.unwrap().unwrap());
-                break;
-            }
-            _ => {}
-        }
+    tests();
+    
+    let stdin = io::stdin();
+    for line in stdin.lock().lines() {
+        BqMachine::parse(line.unwrap());
     }
 }
